c1e0de8d1cadad14767cbcd2a03e202b44fcb1d5
Night-Owl-10 <pranavramteke40@gmail.com>
2024-11-10 14:22:05 +0530

    Assignment corrected

--- index.js
+++ index.js
@@ -4,29 +4,38 @@ const info = {
     student_name: '',
     student_id: '',
     email: '',
-    class_no: '',
-    roll_no: '',
     contact_no: '',
 }
+
 
 const getData = () => {
     info.student_name = document.getElementById('name').value;
     info.student_id = document.getElementById('id').value;
     info.email = document.getElementById('email').value;
-    info.class_no = document.getElementById('classNo').value;
-    info.roll_no = document.getElementById('rollNo').value;
     info.contact_no = document.getElementById('contactNo').value;
 
-
-    if (localStorage.getItem("infoSection") === null) {
-        infoItem = [];
+    if(info.student_name == "" || info.student_id == "" || info.email == "" || info.contact_no == "") {
+        alert("All fields are mandatory");
+    } else if(info.student_id.length > 4) {
+        alert("Student ID must not be greater than 4 digits");
+    }  else if(isNaN(info.contact_no)) {
+        alert("Student ID must not contain any alphabets");
+    } else if(info.contact_no.length < 10 || info.contact_no.length > 10) {
+        alert("Please Enter valid 10 digit Contact No.");
+    }  else if(isNaN(info.contact_no)) {
+        alert("Please Enter valid 10 digit Contact No.");
+    } else {
+        if (localStorage.getItem("infoSection") === null) {
+            infoItem = [];
+        }
+        else {
+            infoItem = JSON.parse(localStorage.getItem("infoSection"))
+        }
+        infoItem.push(info);
+        localStorage.setItem("infoSection", JSON.stringify(infoItem));
     }
-    else {
-        infoItem = JSON.parse(localStorage.getItem("infoSection"))
-    }
-    infoItem.push(info);
-    localStorage.setItem("infoSection", JSON.stringify(infoItem));
 }
+
 
 const showData = () => {
     let cardContainer = document.getElementById("cardContainer");
@@ -48,8 +57,6 @@ const showData = () => {
                 <p><strong>Name</strong> : ${item.student_name}</p>
                 <p><strong>ID</strong> : ${item.student_id}</p>
                 <p><strong>Email</strong> : ${item.email}</p>
-                <p><strong>Class No.</strong> : ${item.class_no}</p>
-                <p><strong>Roll No.</strong> : ${item.roll_no}</p>
                 <p><strong>Contact No.</strong> : ${item.contact_no}</p>
                 <button onclick="deleteData(${index})">Delete</button>
             </div>
@@ -59,16 +66,20 @@ const showData = () => {
     cardContainer.innerHTML = cards;
 }
 
+
 const deleteData = (index) => {
     let getList = JSON.parse(localStorage.getItem("infoSection"));
     getList.splice(index, 1);
 
     localStorage.setItem("infoSection", JSON.stringify(getList));
-    window.location.reload();
+    showData();
+}
+
+window.onload = () => {
+    showData();
 }
 
 submitBtn.addEventListener(('click'), () => {
     getData();
     showData();
 })
-
